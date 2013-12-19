0ec99d1e95327094d322243a85a7ab8af4d65a7c
markoeh <marianne.koehl@laposte.net>
2013-12-19 16:47:20 +0100

    Delete script_scraping.sh

--- Projet_Allocine/script_scraping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- 
-curl -s http://www.allocine.fr/film/aucinema/ grep 'div'
-
-curl -s http://www.allocine.fr/film/aucinema/ | Sed -n '/<a class=\"no_underline\".*/,/<\/a>/p'| sed 's/<[^>]*>//g'
