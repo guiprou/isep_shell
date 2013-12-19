8b6757a57b3a05fa4488e311439cc93abc533977
marianne <mkoehl@MacBook-Pro-de-Marianne.local>
2013-12-19 16:46:32 +0100

    Merge branch 'master' of https://github.com/markoeh/isep_shell
    Conflicts:
    	Projet_Allocine/script_scraping.sh

--- Projet_Allocine/script_scraping.sh
+++ Projet_Allocine/script_scraping.sh
@@ -1,1 +1,5 @@
-curl
+#!/bin/bash
+<<<<<<< HEAD
+curl -s http://www.allocine.fr/film/aucinema/ | sed -n '/<a class=\"no_underline\".*/,/<\/a>/p'| sed 's/<[^>]*>//g'curl
+=======
+curl -s http://www.allocine.fr/film/aucinema/ | Sed -n '/<a class=\"no_underline\".*/,/<\/a>/p'| sed 's/<[^>]*>//g'
