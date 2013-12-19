0758b91623e12ceb7379b4644be2335be6ffe070
markoeh <marianne.koehl@laposte.net>
2013-12-19 16:50:11 +0100

    Update script_scraping.sh

--- Projet_Allocine/script_scraping.sh
+++ Projet_Allocine/script_scraping.sh
@@ -2,8 +2,7 @@
  
 <<<<<<< HEAD
 curl -s http://www.allocine.fr/film/aucinema/ | sed -n '/<a class=\"no_underline\".*/,/<\/a>/p'| sed 's/<[^>]*>//g'curl
-=======
-curl -s http://www.allocine.fr/film/aucinema/ grep 'div'
+
 
 curl -s http://www.allocine.fr/film/aucinema/ | Sed -n '/<a class=\"no_underline\".*/,/<\/a>/p'| sed 's/<[^>]*>//g'
 >>>>>>> 95cbac4c5186232bb4c8b9305b653edf935e7c4b
