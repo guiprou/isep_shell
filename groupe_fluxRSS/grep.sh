3c596725c097c5b5ef5bdb28dbb47d8a19eec93f
AyakoD <elinaewen0o@hotmail.fr>
2014-01-08 14:54:41 +0100

    Update grep.sh

--- groupe_fluxRSS/grep.sh
+++ groupe_fluxRSS/grep.sh
@@ -1,3 +1,8 @@
-curl "http://www.sport.fr/RSS/sport1.xml" | grep -E '(title>|description>)' | \
-awk '/<title>/,/<\/title>/' |  sed "s/<title>//g" | sed "s/<\/title>/\n/g" | sed 's/^[ \t]*//' > simple.txt 
-awk '/<description>/,/<\/description>/' |  sed "s/<description>//g" | sed "s/<\/description>/\n/g" | sed 's/^[ \t]*//' >> simple.txt 
+#!/bin/bash
+
+url_flux="http://www.sport.fr/RSS/sport1.xml"
+
+
+echo "--flux--"
+curl -s $url_flux | grep "<title>" | sed -e 's/<\/title>//g' -e 's/<title>//g' 
+echo
